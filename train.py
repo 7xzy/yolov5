0692aeb78d0c213680ba5214fb9b570bcfe70af8
7xzy <83130962+7xzy@users.noreply.github.com>
2023-03-17 18:32:06 +0100

    ciao

--- train.py
+++ train.py
@@ -307,6 +307,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
             with torch.cuda.amp.autocast(amp):
                 pred = model(imgs)  # forward
                 loss, loss_items = compute_loss(pred, targets.to(device))  # loss scaled by batch_size
+                print(loss)
                 if RANK != -1:
                     loss *= WORLD_SIZE  # gradient averaged between devices in DDP mode
                 if opt.quad:
